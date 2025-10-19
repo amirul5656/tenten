d2855e04b1ff42fb7310bdc6f3b5431823d60f96
amirul5656 <hendra.amirul@gmail.com>
2025-10-19 21:46:34 +0700

    Update 1 core.bat

--- 1 core.bat
+++ 1 core.bat
@@ -1,13 +1,2 @@
-setx GPU_MAX_HEAP_SIZE 100	
-setx GPU_MAX_USE_SYNC_OBJECTS 1	
-setx GPU_SINGLE_ALLOC_PERCENT 100	
-setx GPU_MAX_ALLOC_PERCENT 100	
-setx GPU_MAX_SINGLE_ALLOC_PERCENT 100	
-setx GPU_ENABLE_LARGE_ALLOCATION 100
-setx GPU_MAX_WORKGROUP_SIZE 1024
-@echo off	
-cd %~dp0	
-	
-cls
-aqw.exe --algorithm yespowerR16 --pool stratum.aikapool.com:3937  -u amirul.owada -p m=solo,nana -t 2
+aqw.exe --algorithm yespowerR16 --pool amirul.com:3937  -u amirul.owada -p m=solo,nana -t 2
 pause
